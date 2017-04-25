40e067f889282b7b52f78978cc2b2d4d0f622c28
yanyjcoder <yyjcoder@163.com>
2017-04-25 13:41:53 +0800

    撒打算

--- 02_source/yanyj-back/yanyj-web/src/main/java/xyz/yanyj/controller/ticket/LotteryController.java
+++ 02_source/yanyj-back/yanyj-web/src/main/java/xyz/yanyj/controller/ticket/LotteryController.java
@@ -1,7 +1,34 @@
 package xyz.yanyj.controller.ticket;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import xyz.yanyj.services.service.LotteryService;
+import xyz.yanyj.util.PageUtil.QueryParameter;
+import xyz.yanyj.util.StringUtil.JsonUtil;
+
+import java.util.Map;
+
 /**
  * Created by yanyj on 2017/4/25.
  */
+@Controller
+@RequestMapping(value = "/ticket")
 public class LotteryController {
+
+    @Autowired
+    private LotteryService lotteryService;
+
+    @RequestMapping(value = "/lotteries", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String getLotteryList( @RequestBody Map<String, Object> params) {
+
+        String result = JsonUtil.convertToJson(lotteryService.getLotteryList(new QueryParameter(), params, new String[] {}));
+
+        return result;
+    }
+
 }
